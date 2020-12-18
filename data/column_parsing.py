f038c69e66a7e1cb8cbbc4185c2bc1fe9e0a68e3
Lewis Cowles <lewiscowles@me.com>
2020-12-18 14:28:50 +0000

    Use Pandas & Numpy fwf
    It occurrd to me that my choice not to use Pandas
    may not go over well.
    
    I did it because of the short time.
    Here is a branch using Pandas & Numpy

--- data/column_parsing.py
+++ data/column_parsing.py
@@ -1,4 +1,7 @@
 import re
+import numpy as np
+import pandas as pd
+from io import StringIO
 
 
 pattern = re.compile("([ ]{2,}){1,4}")
@@ -16,3 +19,24 @@ def fix_double_space_in_second_column(line):
     if len(line) == 5:
         line = [line[0], line[1] + " " + line[2], line[3], line[4]]
     return line
+
+
+def pandas_fixed_width_parsing(columnRawData):
+    fakeFile = StringIO(columnRawData)
+    df = pd.read_fwf(fakeFile, "infer", (16, 30, 16, 11)).replace({np.nan: None})
+    out = df.values.tolist()
+    out.insert(0, df.columns.tolist())
+    return out
+
+
+def fixed_width_third_column_date_in_first_column(columnData):
+    all_other_columns_nan = all(
+        [
+            columnData[-1][0],
+            not all([columnData[-1][1], columnData[-1][2], columnData[-1][3]]),
+        ]
+    )
+    if all_other_columns_nan:
+        columnData[-1][2] = columnData[-1][0]
+        columnData[-1][0] = columnData[-1][1]
+    return columnData
--- data/column_parsing_test.py
+++ data/column_parsing_test.py
@@ -3,6 +3,8 @@ import pytest
 from .column_parsing import (
     line_parse_regex_split,
     fix_double_space_in_second_column,
+    pandas_fixed_width_parsing,
+    fixed_width_third_column_date_in_first_column,
 )
 
 
@@ -40,3 +42,57 @@ def test_helper_fixes_incorrect_column_count(lineIndex, description):
     assert len(result) == 5
     fixed = fix_double_space_in_second_column(result)
     assert len(fixed) == 4
+
+
+def test_pandas_raw_parsing_fwf():
+    rawData = """19.07.2006      10 Century House, (second     20.06.2006      SY751695   
+2 (part of and  floor flat)                   199 years from             
+4 (part of)                                   20.6.2006"""
+    data = pandas_fixed_width_parsing(rawData)
+    for line in data:
+        assert len(line) == 4
+    assert data[0][0] == "19.07.2006"
+    assert data[0][1] == "10 Century House, (second"
+    assert data[0][2] == "20.06.2006"
+    assert data[0][3] == "SY751695"
+    assert data[1][0] == "2 (part of and"
+    assert data[1][1] == "floor flat)"
+    assert data[1][2] == "199 years from"
+    assert data[2][0] == "4 (part of)"
+    assert data[2][2] == "20.6.2006"
+
+
+def test_pandas_raw_parsing_fwf_3rd_column_as_1st():
+    rawData = """19.07.2006      9 Century House (second       20.06.2006      SY751694   
+2 (part of)     floor flat)                   199 years from             
+20.6.2006"""
+    data = pandas_fixed_width_parsing(rawData)
+    for line in data:
+        assert len(line) == 4
+    assert data[0][0] == "19.07.2006"
+    assert data[0][1] == "9 Century House (second"
+    assert data[0][2] == "20.06.2006"
+    assert data[0][3] == "SY751694"
+    assert data[1][0] == "2 (part of)"
+    assert data[1][1] == "floor flat)"
+    assert data[1][2] == "199 years from"
+    assert data[2][0] == "20.6.2006"
+
+
+def test_pandas_fix_missing_column_date_repeat_in_first():
+    rawData = """19.07.2006      9 Century House (second       20.06.2006      SY751694   
+2 (part of)     floor flat)                   199 years from             
+20.6.2006"""
+    data = pandas_fixed_width_parsing(rawData)
+    for line in data:
+        assert len(line) == 4
+    assert data[0][0] == "19.07.2006"
+    assert data[0][1] == "9 Century House (second"
+    assert data[0][2] == "20.06.2006"
+    assert data[0][3] == "SY751694"
+    assert data[1][0] == "2 (part of)"
+    assert data[1][1] == "floor flat)"
+    assert data[1][2] == "199 years from"
+    assert data[2][0] == "20.6.2006"
+    data = fixed_width_third_column_date_in_first_column(data)
+    assert data[2][2] == "20.6.2006"
