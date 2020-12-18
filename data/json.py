fd0a85c8a3516447d79dc3c721a481049a7e813f
Lewis Cowles <lewiscowles@me.com>
2020-12-18 10:19:16 +0000

    Split notes from column data

--- data/json.py
+++ data/json.py
@@ -1,6 +1,10 @@
 import json
 
 
+CANCELLED_ITEM = "Cancelled Item - Schedule of Notices of Leases"
+NOTE_PREFIX = "NOTE"
+
+
 class JsonBaseException(BaseException):
     pass
 
@@ -23,3 +27,31 @@ def simplify_collection(data):
     """
     raw = [item.get("leaseschedule", {}).get("scheduleEntry", None) for item in data]
     return [item for item in raw if item]
+
+
+def raw_parse_item(entryIndex, itemIndex, itemRawText, itemEntryType):
+    out = {}
+    if itemEntryType != CANCELLED_ITEM:
+        itemTextSections = itemRawText.split(NOTE_PREFIX)
+        itemTextSections.reverse()
+        nonNotes = itemTextSections.pop()
+        itemTextSections.reverse()
+        notes = [NOTE_PREFIX + note for note in itemTextSections if note]
+        out = {
+            "entryIndex": entryIndex,
+            "itemIndex": itemIndex,
+            "notes": notes,
+            "columnsText": nonNotes,
+            "itemRawText": itemRawText,
+        }
+    else:
+        out = {
+            "entryIndex": entryIndex,
+            "itemIndex": itemIndex,
+            "notes": [
+                itemRawText
+            ],
+            "columnsText": "",
+            "itemRawText": itemRawText,
+        }
+    return out
--- data/json_test.py
+++ data/json_test.py
@@ -1,7 +1,22 @@
 import pickle
 import pytest
 
-from .json import load_json, simplify_collection, JsonLoadException
+from .json import (
+    load_json,
+    simplify_collection,
+    raw_parse_item,
+    JsonLoadException,
+    CANCELLED_ITEM,
+)
+
+
+SAMPLE_CANCELLED_BODY = "ITEM CANCELLED on 14 February 2019."
+SAMPLE_NOTE = "NOTE: As to the parking space only the surface is included in the lease."
+SAMPLE_BODY = """08.01.2016      14 Adkins Close (ground       18.12.2015      SF613060   
+16 (part of) &  floor flat)                   125 years from             
+17                                            01.01.2015                 
+"""
+SAMPLE_RAW_BODY = SAMPLE_BODY + SAMPLE_NOTE
 
 
 def test_load_file_fail():
@@ -27,3 +42,42 @@ def test_load_file_call():
 )
 def test_simplify_collection(input, expectation):
     assert pickle.dumps(simplify_collection(input)) == pickle.dumps(expectation)
+
+
+@pytest.mark.parametrize(
+    (
+        "inputText",
+        "inputType",
+        "expectation",
+    ),
+    [
+        (
+            SAMPLE_CANCELLED_BODY,
+            CANCELLED_ITEM,
+            {
+                "notes": [SAMPLE_CANCELLED_BODY],
+                "columnsText": "",
+                "itemsRawText": SAMPLE_CANCELLED_BODY,
+            },
+        ),
+        (
+            SAMPLE_RAW_BODY,
+            "Schedule of Notices of Leases",
+            {
+                "notes": [SAMPLE_NOTE],
+                "columnsText": SAMPLE_BODY,
+                "itemsRawText": SAMPLE_RAW_BODY,
+            },
+        ),
+    ],
+)
+def test_raw_parse_item(inputText, inputType, expectation):
+    result = raw_parse_item(1, 1, inputText, inputType)
+    assert "entryIndex" in result
+    assert "itemIndex" in result
+    assert "notes" in result
+    assert "columnsText" in result
+    assert "itemRawText" in result
+    assert result["notes"] == expectation["notes"]
+    assert result["columnsText"] == expectation["columnsText"]
+    assert result["itemRawText"] == inputText
